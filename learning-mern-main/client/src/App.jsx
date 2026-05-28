24c4cad6cc1890c90b76e7d95b2087a34b3ce968
Minchu Kariappa <mkariappak@gmail.com>
2026-05-28 10:58:14 +0530

    Added useRef hook demo component

---
+++ learning-mern-main/client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import UseRefDemo from "./components/UseRefDemo";
+
+const App = () => {
+  return (
+    <div>
+      <UseRefDemo />
+    </div>
+  );
+};
+
+export default App;
